749347ef77e99e00511873e1339fc524d382e881
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-11-06 17:41:03 +0300

    added changes to views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,5 @@
 from flask import render_template,redirect,url_for
+from werkzeug.exceptions import abort
 from wtforms.i18n import messages_path
 from .import main
 from..import db,photos
@@ -20,4 +21,38 @@ def index():
 @login_required
 def new_pitch():
 
-    form = PitchForm
+    form = PitchForm()
+
+    if form.validate_on_submit():
+        category = form.category.data
+        pitch = form.pitch.data
+        comment = form.comment.data
+
+        new_pitch = pitches(title = title,category =category,pitch = pitch,user_id=current_user_id)
+        title = 'New pitch'
+        new_pitch.save_pitch()
+
+        return redirect(url_for('main.index'))
+    return render_template('pitch.html',pitch_entry = form)
+
+@main.route('/user/<uname>')
+def category(cate):
+     '''
+     returns pitches byt category
+     '''
+     category = pitches.get_pitches(cate)
+     title = f'{cate}'
+     return render_template('categories.html',title = title, category = category)
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(author = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+
+    
